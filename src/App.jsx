5e816dd9200e44a7533fea76809f0578936e4622
Firdavs Temirov <Firdavs>
2022-04-27 17:06:48 +0500

    add local store

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Form from "./components/Form/Form";
+import { v4 as uuidv4 } from "uuid";
 import List from "./components/List/List";
-import Buttons from "./components/Buttons/Buttons";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const todoStore = JSON.parse(localStorage.getItem("todoStore"));
+    if (todoStore) setTodos(todoStore)
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoStore", JSON.stringify(todos));
+  }, [todos]);
+  
+
+  const handleAddTodo = (evt) => {
+    if (evt.keyCode === 13) {
+      let newTodo = {
+        id: uuidv4(),
+        text: evt.target.value,
+        isComlate: false,
+      };
+
+      setTodos([newTodo, ...todos]);
+      evt.target.value = "";
+    }
+  };
+
   return (
     <div className="App">
       <section className="todo">
         <div className="container">
-          <div className="todo__wrapper w-100">
-            <Form
-              input={input}
-              setInput={setInput}
-              todos={todos}
-              setTodos={setTodos}
-            />
-            <List todos={todos} setTodos={setTodos}/>
-            <Buttons todos={todos} setTodos={setTodos}/>
+          <h3 className="todo__heading">ToDo-List</h3>
+
+          <div className="todo__wrapper">
+            <div className="input-group mb-3 mt-3 w-50">
+              <input
+                type="text"
+                className="form-control todo__input"
+                name="text"
+                placeholder="Type Todo ..."
+                required
+                aria-describedby="basic-addon1"
+                onKeyUp={handleAddTodo}
+              />
+            </div>
+
+            <List todos={todos} setTodos={setTodos} />
           </div>
         </div>
       </section>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,39 +2,60 @@ import React from "react";
 import "./List.css";
 
 const List = ({ todos, setTodos }) => {
-  const handleComplate = (todo) => {
-    todos.map((item) => {
-      if (item.id === todo.id) {
-        return { ...item, isComlated: !item.isComlated };
-      }
-      return item;
-    });
-  };
-
   const handleDelete = ({ id }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (todo) => {
+    let todoText = prompt("Textni ozgartiring", todo.text);
+
+    todos.forEach((itemTodos) => {
+      if (itemTodos.id === todo.id) {
+        itemTodos.text = todoText;
+      }
+    });
+
+    setTodos([...todos]);
+  };
+
+  const handleComlate = (id) => {
+    todos.forEach((itemTodos) => {
+      if (itemTodos.id === id) {
+        itemTodos.isComlate = !itemTodos.isComlate;
+      }
+    });
+
+    setTodos([...todos]);
   };
 
   return (
     <ul className="todo__list">
       {todos.map((todo) => (
-        <li key={todo.id}>
+        <li key={todo.id} className={todo.isComlate ? "active" : ''}>
           <div className="alert alert-primary form__alert" role="alert">
             <input
               type="checkbox"
               className="form-check-input form__lisrt-input checkbox-btn"
-              onClick={() => handleComplate(todo)}
+              onChange={() => handleComlate(todo.id)}
+              checked={todo.isComlate}
             />
+
             <div className="alert-elemet__wrap">
-              <button
-                className="btn btn-outline-primary delete-btn"
-                onClick={() => handleDelete(todo)}
-              >
-                🗑️
-              </button>
-              <p className="alert__text">
-                {todo.text}
-              </p>
+              <div className="buttins_wrap">
+                <button
+                  className="btn btn-outline-primary edit-btn"
+                  onClick={() => handleEdit(todo)}
+                >
+                  ✏️
+                </button>
+                <button
+                  className="btn btn-outline-primary delete-btn"
+                  onClick={() => handleDelete(todo)}
+                >
+                  🗑️
+                </button>
+              </div>
+              <p className="alert__text">{todo.text}</p>
             </div>
           </div>
         </li>
